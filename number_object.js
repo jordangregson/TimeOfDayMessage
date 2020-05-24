aa31cbf676f883c8291e8a1af437cbb73d521eca
Jordan Gregson <jgregson2016@students.curry.edu>
2020-05-23 22:47:58 -0400

    Update number_object.js

--- number_object.js
+++ number_object.js
@@ -2,6 +2,7 @@ var num = 99.5678;
 
 var msg = "<p>The number is: " + num + "</p>";
 msg += "<p>The number rounded is: " + num.toFixed(0) + "</p>";
+msg += "<p>The whole number is: " + Math.floor(num) + "</p>";
 
 
 
