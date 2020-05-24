e75fdbaefc575f0c3043c780fdba511904336220
Jordan Gregson <jgregson2016@students.curry.edu>
2020-05-23 21:51:16 -0400

    Added a way to display the screen size

---
+++ screen_size.js
@@ -0,0 +1,8 @@
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+var elWidth = document.getElementById("width");
+elWidth.textContent = "Width: " + width;
+
+var elHeight = document.getElementById("height");
+elHeight.textContent = "Height: " + height;
